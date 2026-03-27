9740b7ad480a02978395c83615f7f27ddd2f202f
KaoOat15120 <veerachitptpks@gmail.com>
2026-03-27 13:08:12 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 🔥 required for Docker
+    port: 8080,
+    proxy: {
+      "/api": "http://api:3000", // 🔥 use service name
+    },
+  },
 })
