343d333f2ad2709610caaec46f29dce66e12c935
KaoOat15120 <veerachitptpks@gmail.com>
2026-04-01 02:19:59 +0700

    database

--- lostfound.sql
+++ lostfound.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2026 at 09:11 PM
+-- Generation Time: Mar 31, 2026 at 09:18 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `claims`
+-- Table structure for table `users`
 --
 CREATE DATABASE lostfound;
 USE lostfound;
@@ -32,89 +32,6 @@ USE lostfound;
 
 
 
-
-CREATE TABLE `claims` (
-  `id` int(11) NOT NULL,
-  `item_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `message` text DEFAULT NULL,
-  `status` varchar(50) DEFAULT 'submitted'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `claims`
---
-
-INSERT INTO `claims` (`id`, `item_id`, `user_id`, `message`, `status`) VALUES
-(2, 1, 2, NULL, 'submitted');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
-
-CREATE TABLE `items` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `description` text DEFAULT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  `location` varchar(255) DEFAULT NULL,
-  `status` varchar(50) DEFAULT 'open'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `items`
---
-
-INSERT INTO `items` (`id`, `user_id`, `title`, `description`, `type`, `location`, `status`) VALUES
-(1, 1, NULL, 'aaa', 'lost', 'aaa', 'open'),
-(2, 1, NULL, 'bbb', 'found', 'bbb', 'open');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
-
-CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
-  `thread_id` int(11) DEFAULT NULL,
-  `sender_id` int(11) DEFAULT NULL,
-  `text` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `notifications`
---
-
-CREATE TABLE `notifications` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `claim_id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `fullName` varchar(255) DEFAULT NULL,
-  `contactEmail` varchar(255) DEFAULT NULL,
-  `contactNumber` varchar(50) DEFAULT NULL,
-  `status` enum('pending','approved','rejected') DEFAULT 'pending',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `notifications`
---
-
-INSERT INTO `notifications` (`id`, `user_id`, `claim_id`, `title`, `fullName`, `contactEmail`, `contactNumber`, `status`, `created_at`) VALUES
-(1, 1, 2, NULL, NULL, 'bbb@gmail.com', NULL, 'pending', '2026-03-31 19:03:18');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -125,40 +42,8 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `email`, `password`, `name`, `phone`) VALUES
-(1, 'aaa@gmail.com', '$2b$10$egaux4dm.778yxt./sauh.EnZEytfo4WrDh65KuJKo4nfaKOfzmji', NULL, NULL),
-(2, 'bbb@gmail.com', '$2b$10$8GqpikerbULo4hzjoAfdqOlDfon.KVw/SrLngvYgV.S1PpF7panf.', NULL, NULL);
-
---
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `claims`
---
-ALTER TABLE `claims`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `items`
---
-ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `notifications`
---
-ALTER TABLE `notifications`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -169,30 +54,6 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `claims`
---
-ALTER TABLE `claims`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `items`
---
-ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `notifications`
---
-ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
